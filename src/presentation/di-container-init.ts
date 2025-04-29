59678a11eb775fde73df2325409d6aa02d85dfd9
Frito <fritogotlayed@gmail.com>
2025-04-29 11:19:54 -0400

    chore: testing fixes and cleanup

--- src/presentation/di-container-init.ts
+++ src/presentation/di-container-init.ts
@@ -1,4 +1,8 @@
+import { Cradle } from '@fastify/awilix';
 import { asFunction, AwilixContainer, Lifetime } from 'awilix';
+import { FastifyInstance } from 'fastify';
+import { ContainerManager } from '../core/container-manager';
+import { Logic } from '../core/logic';
 
 export function diContainerInit({
   diContainer,
@@ -21,42 +25,6 @@ export function diContainerInit({
     containerManager: asFunction(
       () => {
         const manager = new ContainerManager();
-        return manager;
-      },
-      {
-        lifetime: Lifetime.SINGLETON,
-        dispose: (containerManager) => {
-          containerManager.stopMonitor();
-        },
-      },
-    ),
-    logic: asFunction(
-      ({ containerManager }) => {
-        return new Logic({ containerManager });
-      },
-      {
-        lifetime: Lifetime.SCOPED,
-      },
-    ),
-  });
-}
-import { Cradle } from '@fastify/awilix';
-import { ContainerManager } from '../core/container-manager';
-import { Logic } from '../core/logic';
-import { FastifyInstance } from 'fastify';
-
-export function defaultDependencyInjection(
-  diContainer: AwilixContainer<Cradle>,
-) {
-  // We define this outside of the registration functions since the functions
-  // execute the first time the item is requested.
-  const manager = new ContainerManager();
-  manager.startMonitor();
-
-  // Wire things up!
-  diContainer.register({
-    containerManager: asFunction(
-      () => {
         return manager;
       },
       {
