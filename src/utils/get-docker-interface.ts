e8aa8cec46ed2aa45293af2f3b3ec7299ae4718b
Frito <fritogotlayed@gmail.com>
2025-04-30 21:53:00 -0400

    fix: update fastify typebox and make docker socket path configurable

--- src/utils/get-docker-interface.ts
+++ src/utils/get-docker-interface.ts
@@ -1,5 +1,16 @@
 import Docker from 'dockerode';
+import config from "config";
 
 export function GetDockerInterface() {
-  return new Docker({ socketPath: '/var/run/docker.sock' });
+  let socketPath = '/var/run/docker.sock';
+
+  if (process.env.MDS_CLOUD_DOCKER_SOCK) {
+    socketPath = process.env.MDS_CLOUD_DOCKER_SOCK;
+  } else if (config.has('docker.socketPath')) {
+    socketPath = config.get('docker.socketPath');
+  }
+
+  return new Docker({
+    socketPath,
+  });
 }
